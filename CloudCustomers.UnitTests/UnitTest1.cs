868259067a6467ce2eeccafb1311e9efc50882cf
Douglas <douglas-cprdev@hotmail.com>
2022-10-03 20:21:59 -0300

    test: example of parameter in test method

--- CloudCustomers.UnitTests/UnitTest1.cs
+++ CloudCustomers.UnitTests/UnitTest1.cs
@@ -7,4 +7,12 @@ public class UnitTest1
     {
 
     }
+
+    [Theory]
+    [InlineData("param1", 1)]
+    [InlineData("param2", 1)]
+    public void Test2(string param1, string param2) {
+
+    }
+
 }
